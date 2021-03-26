bb3556efa42bc0d18669cb95ee6c9f5993bd2448
bhargav-sarvaria <bhargav.sarvaria@gmail.com>
2021-03-26 18:44:12 +0530

    removed aws keys\

--- key_config.py
+++ key_config.py
@@ -4,9 +4,7 @@ Created on Thu Jul  9 19:56:55 2020
 @author: hp
 """
 
-
-ACCESS_KEY_ID='AKIAJOUHBQLVQWTWGZMQ'
-ACCESS_SECRET_KEY='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'
+test = ''
 
 
 #BUCKET_NAME='unigrade-test-bucket-test'
